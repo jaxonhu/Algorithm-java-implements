5764c872568308f9900b4f2e036796acea68c783
jaxonhu <hujiaxuan95@qq.com>
2019-10-23 11:33:34 +0800

    区间查询

--- src/main/java/binary_search/IntervalSearch.java
+++ src/main/java/binary_search/IntervalSearch.java
@@ -1,5 +1,7 @@
 package binary_search;
 
+import java.util.List;
+
 /**
  * @Author: jaxon
  * @Description:
@@ -10,25 +12,29 @@ package binary_search;
 public class IntervalSearch {
 
     public static int getSectionLeft(int leftK, int[] nums) {
-        int low = 0, high = nums.length - 1;
-        while(low < high) {
-            int mid = (low + high) / 2;
-            int temp = nums[mid];
-            if(temp < leftK) {
+        int low = 0, high = nums.length - 1, mid;
+        while (low <= high && leftK >= nums[low] && leftK <= nums[high]) {
+//            mid = (low + high) / 2 + (low + high) % 2;
+            if (low == high)
+            {
+                if (nums[low] == low) return low;
+                return -1;
+            }
+            mid = low + (int)((high - low) * ((float)(leftK - nums[low]) / (float)(nums[high] - nums[low])));
+            if (nums[mid] < leftK) {
                 low = mid + 1;
-            } else {
-                high = mid;
+            } else if(nums[mid] > leftK){
+                high = mid - 1;
+            }else {
+                return mid;
             }
+            System.out.println("low: " + low + " high" + high);
         }
-        if(high > 0 && nums[high] != leftK) {
-            high--;
-            while(high > 0) {
-                if(nums[high] == nums[high-1])
-                    high --;
-                else
-                    break;
-            }
-        }
+//        if(high > 0 && nums[high] > leftK) {
+//            high--;
+//        }
+        if(leftK < nums[high])
+            high --;
         return high;
     }
 
@@ -56,10 +62,14 @@ public class IntervalSearch {
 
     public static void main(String[] args) {
 
-        int[] nums = {0,0,0,0,2,2,2,2,3,4,5,5,5,7,9,9,9,10};
+        List<Integer> r = null;
+        r = null;
+        int[] nums1 = {0, 202, 404, 606, 808, 1010, 1212, 1414, 1616, 1818, 2020};
+        int[] nums2 = {5000, 5202, 5404, 5606, 5808, 6010, 6212, 6414, 6616, 6818, 7020};
+
 //        int[] nums = {0,2};
-        int left = getSectionLeft(1, nums);
-        int right = getSectionRight(6, nums);
+        int left = getSectionLeft(6413, nums2);
+        int right = getSectionRight(27, nums2);
         System.out.println("left: " + left);
         System.out.println("right: " + right);
     }
