5515862db82516d1d3facbab82dcd767d013784d
jaxonhu <hujiaxuan95@qq.com>
2019-12-11 15:11:19 +0800

    241. Different Ways to Add Parentheses

--- src/main/java/divideconquer/Parentheses.java
+++ src/main/java/divideconquer/Parentheses.java
@@ -45,6 +45,53 @@ public class Parentheses {
         return result;
     }
 
+    /**
+     *  dp算法: 核心就是填递推表格
+     * @param input
+     * @return
+     */
+    public List<Integer> diffWaysToCompute2(String input) {
+        List<Integer> result=new ArrayList<>();
+        if(input==null||input.length()==0)  return result;
+        List<String> ops=new ArrayList<>();
+        for(int i=0; i<input.length(); i++){
+            int j=i;
+            while(j<input.length()&&Character.isDigit(input.charAt(j)))
+                j++;
+            ops.add(input.substring(i, j));
+            if(j!=input.length())   ops.add(input.substring(j, j+1));
+            i=j;
+        }
+        int N=(ops.size()+1)/2; //num of integers
+        ArrayList<Integer>[][] dp=(ArrayList<Integer>[][]) new ArrayList[N][N];
+        for(int d=0; d<N; d++){
+            if(d==0){
+                for(int i=0; i<N; i++){
+                    dp[i][i]=new ArrayList<>();
+                    dp[i][i].add(Integer.valueOf(ops.get(i*2)));
+                }
+                continue;
+            }
+            for(int i=0; i<N-d; i++){
+                dp[i][i+d]=new ArrayList<>();
+                for(int j=i; j<i+d; j++){
+                    ArrayList<Integer> left=dp[i][j], right=dp[j+1][i+d];
+                    String operator=ops.get(j*2+1);
+                    for(int leftNum:left)
+                        for(int rightNum:right){
+                            if(operator.equals("+"))
+                                dp[i][i+d].add(leftNum+rightNum);
+                            else if(operator.equals("-"))
+                                dp[i][i+d].add(leftNum-rightNum);
+                            else
+                                dp[i][i+d].add(leftNum*rightNum);
+                        }
+                }
+            }
+        }
+        return dp[0][N-1];
+    }
+
     public static void main(String[] args) {
         String input = "11";
         List<Integer> res = diffWaysToCompute(input);
