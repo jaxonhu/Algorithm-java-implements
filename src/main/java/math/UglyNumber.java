7398c4996057c3cfbc41d4b4567742a1b2c0689a
jaxonhu <370403444@qq.com>
2020-05-12 22:55:08 +0800

    refine

--- src/main/java/math/UglyNumber.java
+++ src/main/java/math/UglyNumber.java
@@ -37,4 +37,23 @@ public class UglyNumber {
        }
        return store[index-1];
     }
+
+    /**
+     *  动态规划解法
+     */
+    public int nthUglyNumber(int n) {
+        int[] dp = new int[n];
+        dp[0] = 1;
+        int n2 = 0, n3 = 0, n5 = 0;
+        for(int i = 1 ; i < n ; i ++) {
+            int nx2 = dp[n2] * 2;
+            int nx3 = dp[n3] * 3;
+            int nx5 = dp[n5] * 5;
+            dp[i] = Math.min(nx2, Math.min(nx3, nx5));
+            if(dp[i] == nx2) n2 ++;
+            if(dp[i] == nx3) n3 ++;
+            if(dp[i] == nx5) n5 ++;
+        }
+        return dp[n-1];
+    }
 }
