5efb86d614dbfdb8b14f36fdbf46d6580c14d3c9
jaxonhu <370403444@qq.com>
2020-05-13 21:42:25 +0800

    另一种解法

--- src/main/java/dynamicprogramming/BestTimeToBuyAndSellStock.java
+++ src/main/java/dynamicprogramming/BestTimeToBuyAndSellStock.java
@@ -66,4 +66,29 @@ public class BestTimeToBuyAndSellStock {
         BestTimeToBuyAndSellStock instance = new BestTimeToBuyAndSellStock();
         System.out.println(instance.maxProfit2(input));
     }
+
+    /**
+     *
+     * dp状态表示
+     * dp[i][j][k] 表示第i天，交易进行了j次，当前k=0 未持有 k=1持有的利润
+     *
+     */
+    public int maxProfit3(int[] prices) {
+        int n = prices.length;
+        int[][][] dp = new int[n][3][2]; // 0 清空  1 持有
+        int res = 0;
+        if(n == 0) return 0;
+        for(int i = 0 ; i < n ; i ++) {
+            for(int j = 1 ; j < 3 ; j ++) {
+                if(i == 0) {
+                    dp[i][j][1] -= prices[i];
+                    dp[i][j][0]  = 0;
+                    continue;
+                }
+                dp[i][j][0]  = Math.max(dp[i-1][j][0], dp[i-1][j][1] + prices[i]);
+                dp[i][j][1] = Math.max(dp[i-1][j-1][0] - prices[i], dp[i-1][j][1]);
+            }
+        }
+        return dp[n-1][2][0];
+    }
 }
