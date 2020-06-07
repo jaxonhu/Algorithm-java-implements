3eb7d6fa0908786c306a10f4809a5d0a329d2d59
jaxonhu <370403444@qq.com>
2020-06-07 12:04:19 +0800

    leetcode weekly contest

---
+++ src/main/java/stringtype/ValidNumber.java
@@ -0,0 +1,11 @@
+package stringtype;
+
+/**
+ * @ Author: jaxon
+ * @ Description:
+ * @ Date:  2020/5/27
+ * @ Time: 3:28 下午
+ * @ Project: Algorithm-Java-implements
+ */
+public class ValidNumber {
+}
