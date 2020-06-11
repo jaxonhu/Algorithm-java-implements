95cfc9af3eb54c3deac1c596c0dbb4b2a2eeaec1
jaxonhu <370403444@qq.com>
2020-06-11 23:06:00 +0800

    update

--- src/main/java/concurrent/ThreadsSum.java
+++ src/main/java/concurrent/ThreadsSum.java
@@ -9,6 +9,12 @@ package concurrent;
  */
 public class ThreadsSum {
 
+    /**
+     *
+     * 面试一道手写volatile的例题，当时很奇怪加不加volatile关键字 除了第一次运行，都能得到正确的结果
+     * 当时怎么改线程数、循环数都不行，现在看来，可能是牛客网锁了线程数/核心数。
+     *
+     */
 
     public static  int sum = 0;
 
